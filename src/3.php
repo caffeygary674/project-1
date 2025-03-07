034e42473400cac4a90ad9cd00bcb118983330cb
caffeygary674 <caffeygary674@gmail.com>
2025-03-07 14:34:07 +0000

    Add 3.php

---
+++ src/3.php
@@ -0,0 +1,8 @@
+// This is a PHP comment
+$x = 5; // Declare a variable called $x and set it to 5
+
+if ($x > 10) {
+    echo "The number is greater than 10";
+} else {
+    echo "The number is less than or equal to 10";
+}
