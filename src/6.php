2afbc0a8e5ad6d0985581a24e93eba6ca8e604a0
caffeygary674 <caffeygary674@gmail.com>
2025-03-10 14:48:25 +0000

    Add 6.php

---
+++ src/6.php
@@ -0,0 +1,13 @@
+
+<?php
+
+// Define an array of colors
+$colors = array("red", "blue", "green", "yellow");
+
+// Randomly select one color from the array
+$color = $colors[array_rand($colors)];
+
+// Output the selected color
+echo $color;
+
+?>
